9f1a4397c2a93c09213ed61f8cd175894c016a23
Edgar Post <info@edgarpost.com>
2016-06-19 08:25:36 +0200

    simple detect env

--- src/app/config.js
+++ src/app/config.js
@@ -1,24 +1,29 @@
-import * as appTypes from '~/enum/app-types'
-import isMobileOrTablet from '~/util/mobile-check'
-import _ from 'lodash';
-const env = 'dev';
+import * as appTypes from "~/enum/app-types";
+import isMobileOrTablet from "~/util/mobile-check";
+import _ from "lodash";
+
+const pathName = window.location.toString();
+
+let env = 'prod';
+if(window.location.hostname === 'localhost')
+{
+	env = 'dev';
+}
 
 let config = {
 	type: appTypes.TYPE_LIVE,
 	mobile: isMobileOrTablet()
 };
 
-const pathName = window.location.toString();
-
-if(_.includes(pathName, '/manager')) {
+if(_.includes(pathName, '/manager'))
+{
 	config.type = appTypes.TYPE_MANAGER
 }
 
-if(_.includes(pathName, '/view')) {
+if(_.includes(pathName, '/view'))
+{
 	config.type = appTypes.TYPE_VIEWER
 }
-
-console.log(config.type);
 
 switch(env)
 {

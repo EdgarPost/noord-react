8f22f6959611f97808febd831f905326232e4f75
Edgar Post <info@edgarpost.com>
2017-08-23 20:32:50 +0200

    Update date and formatting

--- src/app/actions/google-maps.js
+++ src/app/actions/google-maps.js
@@ -1,14 +1,12 @@
-export const SET_ZOOM_LEVEL = 'SET_ZOOM_LEVEL';
-export const SET_CENTER = 'SET_CENTER';
+export const SET_ZOOM_LEVEL = "SET_ZOOM_LEVEL";
+export const SET_CENTER = "SET_CENTER";
 
-export function setCenter(lat, lng)
-{
-	return {
-		type: SET_CENTER,
-		payload: {
-			lat: parseFloat(lat),
-			lng: parseFloat(lng)
-		},
-	}
+export function setCenter(lat, lng) {
+  return {
+    type: SET_CENTER,
+    payload: {
+      lat: parseFloat(lat),
+      lng: parseFloat(lng)
+    }
+  };
 }
-
